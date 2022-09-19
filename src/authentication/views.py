691d5f9bd55f5a60df8656f8cd548f0e78aeb276
valeriuv <valeriu.vetiul@gmail.com>
2022-09-19 20:12:34 +0300

    Username validation

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.views import View
+import json
+from django.http import JsonResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+        if not str(username).isalnum():
+            return JsonResponse({'username_error': 'Username should only contain alphanumeric characters'}, status=400)
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error': 'Username is already in use.'}, status=409)
+
+        else:
+            return JsonResponse({'username_valid': True})
 
 class RegistrationView(View):
     def get(self, request):
         return render(request, 'authentication/register.html')
+
+
---
+++ src/financeapp/static/js/register.js
@@ -0,0 +1,10 @@
+const usernameField=document.querySelector("#usernameField");
+
+usernameField.addEventListener("keyup", (e) => {
+
+    const usernameVal=e.target.value;
+    console.log("usernameVal", usernameVal)
+
+    fetch("/authentication/validate-username");
+
+})
