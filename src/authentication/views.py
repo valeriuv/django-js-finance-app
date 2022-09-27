8f5a55dad89d74e12467b65ece96b41d4be50e39
valeriuv <valeriu.vetiul@gmail.com>
2022-09-27 11:05:28 +0300

    Add EmailValidationView

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -3,6 +3,7 @@ from django.views import View
 import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
+import validate_email from validate_email
 
 # Create your views here.
 
@@ -15,6 +16,18 @@ class UsernameValidationView(View):
         if User.objects.filter(username=username).exists():
             return JsonResponse({'username_error': 'Username is already in use.'}, status=409)
         return JsonResponse({'username_valid': True})
+
+
+class EmailValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        email = data['email']
+        if not validate_email(email):
+            return JsonResponse({'email_error': 'Email is invalid.'}, status=400)
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error': 'Email is already in use.'}, status=409)
+        return JsonResponse({'email_valid': True})
+
 
 class RegistrationView(View):
     def get(self, request):
